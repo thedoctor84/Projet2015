d611016af48bb0291505869ecb79eef652874c8a
Antoine Letourneur <ant984@free.fr>
2016-03-23 11:24:49 +0100

    Ajout du fichier EloRankSystem pour l'elo

---
+++ src/fr/univavignon/courbes/stats/EloRankSystem.java
@@ -0,0 +1,9 @@
+package fr.univavignon.courbes.stats;
+
+public class EloRankSystem {
+
+	public void NewElo(int OldElo1, int OldElo2) {
+		// http://setastyle.free.fr/chess/elo.php
+		// http://gobase.org/studying/articles/elo/
+	}
+}
