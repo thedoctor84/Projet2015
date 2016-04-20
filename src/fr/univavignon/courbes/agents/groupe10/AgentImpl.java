fb119aa6171e94c04333f0e91c205de04ba645cc
Antoine Letourneur <ant984@free.fr>
2016-04-20 08:52:58 +0200

    ajout deu package et de la classe principale de l'agent

---
+++ src/fr/univavignon/courbes/agents/groupe10/AgentImpl.java
@@ -0,0 +1,29 @@
+package fr.univavignon.courbes.agents.groupe10;
+
+import fr.univavignon.courbes.agents.Agent;
+import fr.univavignon.courbes.common.Direction;
+
+/**
+ * Agent artificiel du groupe 10
+ * @author uapv1504323
+ *
+ */
+public class AgentImpl extends Agent {
+
+	/**
+	 * Constructeur
+	 * @param playerId
+	 * id du joueur
+	 */
+	public AgentImpl(Integer playerId) {
+		super(playerId);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Direction processDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
